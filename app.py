4e455d629bad3e198ee56dbdd7df37659bf83901
Ubuntu <ubuntu@ip-172-31-29-41.ec2.internal>
2019-09-20 09:50:45 +0000

    established connection to MongoDB, with display of content to template books.html

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+MONGODB_URI = os.getenv("MONGO_URI")
+MONGODB_NAME = "book_review_site"
+
+#app.config["MONGO_DBNAME"]
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/get_books")
+def get_books():
+    return render_template("books.html", books=mongo.db.books.find())
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
