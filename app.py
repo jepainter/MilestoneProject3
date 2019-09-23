991665d84d0c6cbb7c8af59818f580bfeb54d2d2
Ubuntu <ubuntu@ip-172-31-29-41.ec2.internal>
2019-09-23 07:03:23 +0000

    added index, addbook html pages and linked to app for display

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+Imports of packages for functioning of site
+"""
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
@@ -6,7 +9,6 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 MONGODB_URI = os.getenv("MONGO_URI")
-MONGODB_NAME = "book_review_site"
 
 #app.config["MONGO_DBNAME"]
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
@@ -14,10 +16,25 @@ app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 
 @app.route("/")
+def home_screen():
+    """
+    Function for landing page
+    """
+    return render_template("index.html")
+
 @app.route("/get_books")
 def get_books():
+    """
+    Function to fetch books and display to html
+    """
     return render_template("books.html", books=mongo.db.books.find())
 
+@app.route("/add_book")
+def add_book():
+    """
+    Function to add a book and review to the database and render to html
+    """
+    return render_template("addbook.html", books=mongo.db.books.find())
 
 
 if __name__ == "__main__":
