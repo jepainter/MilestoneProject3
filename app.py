8df61f9d01110b35e13fae7f42978cad50a708d9
Ubuntu <ubuntu@ip-172-31-87-90.ec2.internal>
2019-10-25 09:21:06 +0000

    final upload of project files

--- app.py
+++ app.py
@@ -1359,4 +1359,4 @@ Call to run the app
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
